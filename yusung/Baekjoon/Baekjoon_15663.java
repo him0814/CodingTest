2bf435e05a50ce30c3ca70a5dbc9cdada84c6dd3
BlancCarte <yusung0924@naver.com>
2022-08-11 23:23:33 +0900

    2022.08.11 스터디커밋

--- yusung/Baekjoon/Baekjoon_15663.java
+++ yusung/Baekjoon/Baekjoon_15663.java
@@ -3,7 +3,7 @@ package Baekjoon;
 import java.io.*;
 import java.util.*;
 
-//백준 15657 N과 M(8)
+//백준 15663 N과 M(9)
 //dfs 
 //첫째줄에 N과 M이 주어짐(1<=M<=N<=8)
 //둘째줄에 N개의 수가 주어짐(1<=num<=10000)
@@ -11,8 +11,11 @@ import java.util.*;
 //N개의 자연수 중에서 M개를 고른 수열 출력
 //입력받은 N개의 자연수가 주어짐 이때 M개를 고르는 수열 출력
 //사전순으로 출력해야되므로 배열에 넣고 정렬하면될듯
+//중복된 수가 있어서 중복을 제거해야됨
+//9 7 9 1
+//17 19 71 79 91 97 99
 
-public class Baekjoon_15657 {
+public class Baekjoon_15663 {
 
 	static int N;
 	static int M;
@@ -36,28 +39,39 @@ public class Baekjoon_15657 {
 		}
 		Arrays.sort(now);
 
-		answer = new int[M];
+		answer = new int[N];
 		visit = new boolean[N];
 
 		// 앞에 문제와 다르게 at을 0으로 넘겨줘야됨 왜 why? now배열의 index로 i가 들어가야되기때문
-		dfs(0, 0);
+		dfs(0);
 		sb.setLength(sb.length() - 1);
 		System.out.println(sb.toString());
 	}
 
-	public static void dfs(int depth, int at) {
+	public static void dfs(int depth) {
 		if (depth == M) {
-			for (int i = 0; i < answer.length; i++) {
+			// M만큼 뽑을꺼니까 범위가 M까지가 되어야함
+			for (int i = 0; i < M; i++) {
 				sb.append(answer[i] + " ");
 			}
 			sb.append("\n");
 			return;
 		}
+		
+		int past = 0;
 
 		// 따라서 for문도 범위가 달라져야함
-		for (int i = at; i < N; i++) {
-			answer[depth] = now[i];
-			dfs(depth + 1, i);
+		for (int i = 0; i < N; i++) {	
+			if(visit[i]) {
+				continue;
+			}
+			if (past != now[i]) {
+				visit[i] = true;
+				answer[depth] = now[i];
+				past = now[i];
+				dfs(depth+1);
+				visit[i] = false;
+			}
 		}
 	}
 }
